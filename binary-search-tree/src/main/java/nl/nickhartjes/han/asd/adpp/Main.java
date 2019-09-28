2e9cf4c009fd7b3fc4fc2a625830fe0b88897adf
nickhartjes <info@nickhartjes.nl>
2019-09-28 16:14:15 +0200

    First setup for generic quicksort

--- binary-search-tree/src/main/java/nl/nickhartjes/han/asd/adpp/Main.java
+++ binary-search-tree/src/main/java/nl/nickhartjes/han/asd/adpp/Main.java
@@ -3,6 +3,6 @@ package nl.nickhartjes.han.asd.adpp;
 class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hallo");
+        // Empty
     }
 }
--- sort/src/main/java/nl/nickhartjes/han/asd/adpp/util/Util.java
+++ sort/src/main/java/nl/nickhartjes/han/asd/adpp/util/Util.java
@@ -7,6 +7,7 @@ public class Util {
         throw new UnsupportedOperationException();
     }
 
+    @SuppressWarnings("squid:S106")
     public static void printArray(int[] array) {
         System.out.println(Arrays.toString(array));
     }
