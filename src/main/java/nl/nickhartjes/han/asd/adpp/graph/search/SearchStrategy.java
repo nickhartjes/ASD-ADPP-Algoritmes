c5b5dac1c80b1eda18af9c1eb0af3e6fe27ef978
nickhartjes <info@nickhartjes.nl>
2019-10-11 10:25:42 +0200

    Changed config to gradle

--- src/main/java/nl/nickhartjes/han/asd/adpp/graph/search/SearchStrategy.java
+++ src/main/java/nl/nickhartjes/han/asd/adpp/graph/search/SearchStrategy.java
@@ -2,9 +2,6 @@ package nl.nickhartjes.han.asd.adpp.graph.search;
 
 import nl.nickhartjes.han.asd.adpp.graph.Graph;
 import nl.nickhartjes.han.asd.adpp.graph.GraphPath;
-import nl.nickhartjes.han.asd.adpp.graph.GraphVertex;
-
-import java.util.Map;
 
 public interface SearchStrategy<T> {
     GraphPath<T> searchShortestPath(Graph<T> graph, T sourceVertex, T destinationVertex);
