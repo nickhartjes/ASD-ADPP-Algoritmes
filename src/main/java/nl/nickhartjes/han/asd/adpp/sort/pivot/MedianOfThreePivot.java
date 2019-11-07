6ec91538c929a4403971d97bc8448596be05d79b
Nick Hartjes <info@nickhartjes.nl>
2019-11-07 13:41:04 +0100

    Added more documentation in the README.MD

--- src/main/java/nl/nickhartjes/han/asd/adpp/sort/pivot/MedianOfThreePivot.java
+++ src/main/java/nl/nickhartjes/han/asd/adpp/sort/pivot/MedianOfThreePivot.java
@@ -1,22 +1,30 @@
 package nl.nickhartjes.han.asd.adpp.sort.pivot;
 
 public class MedianOfThreePivot<T extends Comparable> implements SortPivot<T> {
-  @Override
-  public T getPivot(T[] array, int leftIndex, int rightIndex) {
-    MiddlePivot<T> middlePivot = new MiddlePivot<>();
-    return middleOfThree(
-        array[leftIndex], middlePivot.getPivot(array, leftIndex, rightIndex), array[rightIndex]);
-  }
 
-  private T middleOfThree(T first, T middle, T last) {
-    if ((first.compareTo(middle) < 0 && middle.compareTo(last) < 0)
-        || (last.compareTo(middle) < 0 && middle.compareTo(first) < 0)) {
-      return middle;
-    } else if ((middle.compareTo(first) < 0 && first.compareTo(last) < 0)
-        || (last.compareTo(first) < 0 && first.compareTo(middle) < 0)) {
-      return first;
-    } else {
-      return last;
+    /**
+     *
+     * @param array the array to get the pivot from
+     * @param leftIndex left boundry of the array
+     * @param rightIndex right boundry of the array
+     * @return
+     */
+    @Override
+    public T getPivot(T[] array, int leftIndex, int rightIndex) {
+        MiddlePivot<T> middlePivot = new MiddlePivot<>();
+        return middleOfThree(
+                array[leftIndex], middlePivot.getPivot(array, leftIndex, rightIndex), array[rightIndex]);
     }
-  }
+
+    private T middleOfThree(T first, T middle, T last) {
+        if ((first.compareTo(middle) < 0 && middle.compareTo(last) < 0)
+                || (last.compareTo(middle) < 0 && middle.compareTo(first) < 0)) {
+            return middle;
+        } else if ((middle.compareTo(first) < 0 && first.compareTo(last) < 0)
+                || (last.compareTo(first) < 0 && first.compareTo(middle) < 0)) {
+            return first;
+        } else {
+            return last;
+        }
+    }
 }
--- src/main/java/nl/nickhartjes/han/asd/adpp/sort/pivot/MiddlePivot.java
+++ src/main/java/nl/nickhartjes/han/asd/adpp/sort/pivot/MiddlePivot.java
@@ -2,8 +2,14 @@ package nl.nickhartjes.han.asd.adpp.sort.pivot;
 
 public class MiddlePivot<T extends Comparable> implements SortPivot<T> {
 
-  @Override
-  public T getPivot(T[] array, int leftIndex, int rightIndex) {
-    return array[leftIndex + (rightIndex - leftIndex) / 2];
-  }
+    /**
+     * @param array      the array to get the pivot from
+     * @param leftIndex  left boundry of the array
+     * @param rightIndex right boundry of the array
+     * @return
+     */
+    @Override
+    public T getPivot(T[] array, int leftIndex, int rightIndex) {
+        return array[leftIndex + (rightIndex - leftIndex) / 2];
+    }
 }
